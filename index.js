11228078ae3cea43fe100be224a22fb7bbd58459
Luuk <luukbrauckmann@gmail.com>
2023-02-06 16:10:31 +0100

    Nieuwe design met SSR

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-// Importeer express uit de node_modules map
 import express from 'express'
 
-// Maak een nieuwe express app aan
 const app = express()
+const uuid = 'cldenz5o53s900auo8wumyspq'
 
-// Stel ejs in als template engine en geef de 'views' map door
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
-// Gebruik de map 'public' voor statische resources
 app.use(express.static('public'))
 
-// Maak een route voor de index
-app.get('/', function (req, res) {
-  // res.send('Hello World!')
-  res.render('index')
+app.get('/', async (_req, res) => {
+	let content = null
+
+	await fetch(`https://whois.fdnd.nl/api/v1/member?id=${uuid}`)
+		.then((response) => response.json())
+		.then((data) => content = data.member)
+
+	res.render('index', { content })
 })
 
-// Stel het poortnummer in waar express op gaat luisteren
 app.set('port', process.env.PORT || 8000)
 
-// Start express op, haal het ingestelde poortnummer op
-app.listen(app.get('port'), function () {
-  // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+app.listen(app.get('port'), () => console.log(`Application started on http://localhost:${app.get('port')}`))

73fe6612427b4ef5c7812a04a9fcb0c55e455639
Luuk <luukbrauckmann@gmail.com>
2023-02-07 21:17:41 +0100

    Update alles

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import compression from 'compression'
+import helmet from 'helmet'
 
 const app = express()
 const uuid = 'cldenz5o53s900auo8wumyspq'
@@ -6,6 +8,8 @@ const uuid = 'cldenz5o53s900auo8wumyspq'
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
+app.use(compression())
+app.use(helmet())
 app.use(express.static('public'))
 
 app.get('/', async (_req, res) => {

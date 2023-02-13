39e7dc96ec10de810f34d6117ffdf8ec9b1274bc
Luuk <luukbrauckmann@gmail.com>
2023-02-13 13:59:18 +0100

    Verbeterde layout

--- static/script.js
+++ static/script.js
@@ -1,15 +1,15 @@
 const darkSchemePreferred = window.matchMedia('(prefers-color-scheme: dark)').matches
 
 if (darkSchemePreferred) {
-	document.documentElement.classList.add('dark')
-	document.documentElement.classList.remove('light')
+	document.documentElement.classList.add('dark-scheme')
+	document.documentElement.classList.remove('light-scheme')
 }
 
 addEventListener('click', (event) => {
 	const { target } = event
 
-	if (!target.matches('button') || target.id != 'theme-changer') return
+	if (!target.matches('button') || target.id != 'scheme-changer') return
 
-	document.documentElement.classList.toggle('dark')
-	document.documentElement.classList.toggle('light')
+	document.documentElement.classList.toggle('dark-scheme')
+	document.documentElement.classList.toggle('light-scheme')
 })
